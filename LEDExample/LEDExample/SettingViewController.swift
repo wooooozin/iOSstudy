7fac15faa0f9a79cfefbc720d51321d337423a68
wooooozin <wooooozin@gmail.com>
2023-02-11 10:38:19 +0900

    storyboard UI & Assets 추가

---
+++ LEDExample/LEDExample/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  LEDExample
+//
+//  Created by 효우 on 2023/02/11.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
