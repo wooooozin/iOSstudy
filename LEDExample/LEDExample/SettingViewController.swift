89a60f2b25d514ab3533cdc7d00a86db38d30983
wooooozin <wooooozin@gmail.com>
2023-02-14 06:24:05 +0900

    기능 구현하기

--- LEDExample/LEDExample/SettingViewController.swift
+++ LEDExample/LEDExample/SettingViewController.swift
@@ -7,23 +7,88 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+protocol LEDBoardSettingDelegate: AnyObject {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+}
 
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    
+    weak var delegate: LEDBoardSettingDelegate?
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    var ledText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapTextColorButton(_ sender: UIButton) {
+        if sender == self.yellowButton {
+            self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
+        } else if sender == self.purpleButton {
+            self.changeTextColor(color: .purple)
+            self.textColor = .purple
+        } else {
+            self.changeTextColor(color: .green)
+            self.textColor = .green
+        }
     }
-    */
+    
+    @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
+        switch sender {
+        case self.blackButton:
+            self.changeBackgroundColor(color: .black)
+            self.backgroundColor = .black
+        case self.blueButton:
+            self.changeBackgroundColor(color: .blue)
+            self.backgroundColor = .blue
+        case self.orangeButton:
+            self.changeBackgroundColor(color: .orange)
+            self.backgroundColor = .orange
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapSaveButton(_ sender: UIButton) {
+        self.delegate?.changedSetting(
+            text: self.textField.text,
+            textColor: self.textColor,
+            backgroundColor: self.backgroundColor
+        )
+        self.navigationController?.popViewController(animated: true)
+    }
+}
 
+extension SettingViewController {
+    
+    private func changeTextColor(color: UIColor) {
+        self.yellowButton.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleButton.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenButton.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    
+    private func changeBackgroundColor(color: UIColor) {
+        self.blackButton.alpha = color == UIColor.black ? 1 : 0.2
+        self.blueButton.alpha = color == UIColor.blue ? 1 : 0.2
+        self.orangeButton.alpha = color == UIColor.orange ? 1 : 0.2
+    }
+    
+    private func configureView() {
+        if let ledText = ledText {
+            self.textField.text = ledText
+        }
+        self.changeTextColor(color: self.textColor)
+        self.changeBackgroundColor(color: self.backgroundColor)
+    }
 }
--- LEDExample/LEDExample/ViewController.swift
+++ LEDExample/LEDExample/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.contentsLabel.textColor = .yellow
+        self.contentsLabel.text = "텍스트를 설정해주세요"
+        
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
 
 }
 
+extension ViewController: LEDBoardSettingDelegate {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
+}
