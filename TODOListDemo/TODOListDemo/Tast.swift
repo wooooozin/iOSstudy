f8b2174ee6fc63aa719a5fe566942372faaf7195
wooooozin <wooooozin@gmail.com>
2023-02-16 07:17:13 +0900

    기능 구현1

---
+++ TODOListDemo/TODOListDemo/Tast.swift
@@ -0,0 +1,13 @@
+//
+//  Tast.swift
+//  TODOListDemo
+//
+//  Created by 효우 on 2023/02/16.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var isDone: Bool
+}
--- TODOListDemo/TODOListDemo/ViewController.swift
+++ TODOListDemo/TODOListDemo/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default) { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, isDone: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        }
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요"
+        })
+        self.present(alert, animated: true)
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+    
+    
+}
