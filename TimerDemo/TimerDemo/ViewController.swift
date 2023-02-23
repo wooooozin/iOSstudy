eda4d837e08e36672c3e90aed7ad5f3aa685708a
wooooozin <wooooozin@gmail.com>
2023-02-24 06:07:38 +0900

    기능구현

--- TimerDemo/TimerDemo/ViewController.swift
+++ TimerDemo/TimerDemo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 enum TimerStatus {
     case start
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var progressView: UIProgressView!
@@ -25,6 +27,8 @@ class ViewController: UIViewController {
     
     var duaration = 60
     var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0
     
     // MARK: - Lifecycle
     
@@ -38,11 +42,7 @@ class ViewController: UIViewController {
     @IBAction func tapCancelButton(_ sender: UIButton) {
         switch self.timerStatus {
         case .start, .pause:
-            self.timerStatus = .end
-            self.cancelButton.isEnabled = false
-            self.setTimerInfoViewVisble(isHidden: true)
-            self.datePicker.isHidden = false
-            self.toggleButton.isSelected = false
+            self.stopTimer()
         default:
             break
         }
@@ -52,17 +52,24 @@ class ViewController: UIViewController {
         self.duaration = Int(self.datePicker.countDownDuration)
         switch self.timerStatus {
         case .end:
+            self.currentSeconds = duaration
             self.timerStatus = .start
-            self.setTimerInfoViewVisble(isHidden: false)
-            self.datePicker.isHidden = true
+            UIView.animate(withDuration: 0.5) {
+                self.timerLabel.alpha = 1
+                self.progressView.alpha = 1
+                self.datePicker.alpha = 0
+            }
             self.toggleButton.isSelected = true
             self.cancelButton.isEnabled = true
+            self.startTimer()
         case .start:
             self.timerStatus = .pause
             self.toggleButton.isSelected = false
+            self.timer?.suspend()
         case .pause:
             self.timerStatus = .start
             self.toggleButton.isSelected = true
+            self.timer?.resume()
         }
     }
     
@@ -79,6 +86,51 @@ extension ViewController {
     func configureToggleButton() {
         self.toggleButton.setTitle("시작", for: .normal)
         self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+    
+    func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1)
+            self.timer?.setEventHandler(handler: { [weak self] in
+                guard let self = self else { return }
+                self.currentSeconds -= 1
+                let hour = self.currentSeconds / 3600
+                let minutes = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minutes, seconds)
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duaration)
+                UIView.animate(withDuration: 0.5, delay:0) {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                }
+                UIView.animate(withDuration: 0.5, delay:0.5) {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi * 2)
+                }
+                
+                if self.currentSeconds <= 0 {
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false
+        UIView.animate(withDuration: 0.5) {
+            self.timerLabel.alpha = 0
+            self.progressView.alpha = 0
+            self.datePicker.alpha = 1
+            self.imageView.transform = .identity
+        }
+        self.toggleButton.isSelected = false
+        self.timer?.cancel()
+        self.timer = nil
     }
 }
 
